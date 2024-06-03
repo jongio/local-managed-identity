aea7c40fc195021e06e9432c69c05f318822cc0e
Jon Gallant <2163001+jongio@users.noreply.github.com>
2024-06-03 15:04:25 -0700

    Init

---
+++ local-managed-identity/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+string subscriptionId = builder["AZURE_SUBSCRIPTION_ID"];
+if (string.IsNullOrEmpty(subscriptionId))
+{
+    Console.WriteLine("AZURE_SUBSCRIPTION_ID user secret is not set.");
+    return;
+}
+
+var credential = new ManagedIdentityCredential();
+var client = new ArmClient(credential, subscriptionId);
+var subscription = client.GetSubscriptionResource(new ResourceIdentifier($"/subscriptions/{subscriptionId}"));
+
+Console.WriteLine("Listing resource groups in the subscription...");
+
+await foreach (var resourceGroup in subscription.GetResourceGroups().GetAllAsync())
+{
+    Console.WriteLine($"Resource Group Name: {resourceGroup.Data.Name}");
+}
+
+Console.WriteLine("Listing completed.");
+Console.ReadKey();
